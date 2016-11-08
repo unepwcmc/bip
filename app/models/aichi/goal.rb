d9c59c5401632e28e29f98aa1c7330eebc8888e6
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-11-08 23:47:05 +0000

    add aichi targets to homepage

--- app/models/aichi/goal.rb
+++ app/models/aichi/goal.rb
@@ -1,6 +1,6 @@
 class Aichi::Goal < ActiveRecord::Base
   self.table_name = "aichi_goals"
 
-  has_many :aichi_targets, class_name: "Aichi::Target"
+  has_many :aichi_targets, class_name: "Aichi::Target", foreign_key: "aichi_goal_id"
 end
 
