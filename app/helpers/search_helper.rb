931067f033f75144ea76bad6da34ef8580ba65ae
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-11-12 11:28:24 +0000

    make xpath case insensitive when looking for search bit

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,13 +2,15 @@ module SearchHelper
   def search_result result, query
     if result.is_a?(Comfy::Cms::Block)
       doc = Nokogiri::HTML(result.content)
-      text = doc.xpath("//*[contains(text(), '#{query}')]").first.to_s
+
+      translation = "translate(., '#{query.upcase}', '#{query.downcase}')"
+      text = doc.xpath("//p[contains(#{translation}, #{query})]").first.to_s
 
       regex = Regexp.new("(#{params[:q]})", true)
       raw(text.gsub(regex, '<strong>\1</strong>'))
     else
       doc = Nokogiri::HTML(result.content_cache)
-      text = doc.xpath("//p[contains(text(), ' ')]").first&.to_s || ""
+      text = doc.xpath("//p[contains(text(), ' ')]").first.to_s
       raw(text)
     end
   end
