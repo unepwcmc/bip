5802556d28ce105ef49e8f24c58549f5adfa0205
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-11-12 11:01:42 +0000

    show search result body also for comfy pages

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,9 +1,15 @@
 module SearchHelper
   def search_result result, query
-    doc = Nokogiri::HTML(result.content)
-    text = doc.xpath("//*[contains(text(), '#{query}')]").first.to_s
+    if result.is_a?(Comfy::Cms::Block)
+      doc = Nokogiri::HTML(result.content)
+      text = doc.xpath("//*[contains(text(), '#{query}')]").first.to_s
 
-    regex = Regexp.new("(#{params[:q]})", true)
-    raw(text.gsub(regex, '<strong>\1</strong>'))
+      regex = Regexp.new("(#{params[:q]})", true)
+      raw(text.gsub(regex, '<strong>\1</strong>'))
+    else
+      doc = Nokogiri::HTML(result.content_cache)
+      text = doc.xpath("//p[contains(text(), ' ')]").first&.to_s || ""
+      raw(text)
+    end
   end
 end
