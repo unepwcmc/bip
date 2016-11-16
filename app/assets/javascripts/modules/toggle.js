1daa223739d44258e3bd568afd6c65eb5f0ba3e0
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-11-16 10:04:39 +0000

    add content to collapsed sections in indicator page

--- app/assets/javascripts/modules/toggle.js
+++ app/assets/javascripts/modules/toggle.js
@@ -4,12 +4,19 @@ export default {
 
     let toggleId  = $el.data("toggle-trigger");
     let $targetEl = $(`[data-toggle-target='${toggleId}']`);
+    let $targetAltEl = $(`[data-toggle-target-alt='${toggleId}']`);
     let $switchEl = $(`[data-toggle-switch='${toggleId}']`);
 
+    $targetEl.show();
+    $targetAltEl.hide();
 
     $el.click((ev) => {
       ev.preventDefault();
       $targetEl.toggle();
+
+      if($targetAltEl.length > 0) {
+        $targetAltEl.toggle();
+      }
 
       if($switchEl.length > 0) {
         $switchEl.toggleClass("fa-chevron-down fa-chevron-up");
