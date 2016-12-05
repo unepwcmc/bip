12fdf692bde71c1c43bec6a743cc8cfe953d572b
Stuart Watson <stuart.watson@unep-wcmc.org>
2016-12-05 15:57:03 +0000

    Changed linked_dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml .env} 
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public')
 
 
 # Default value for default_env is {}
