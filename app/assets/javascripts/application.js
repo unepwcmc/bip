693a15a227f085f78c78dab2365fe8de45a5110c
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-12-13 10:51:08 +0000

    add manifest.json and icons for mobile awesomeness

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,3 +29,14 @@ $(document).ready(() => {
 
   pagination.perPageSelector($("[data-per-page-selector]"));
 });
+
+// try to install a service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
