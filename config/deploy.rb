ee994697d2ce3c3808c098caaa930dce432ce56b
Stuart Watson <stuart.watson@unep-wcmc.org>
2021-03-10 11:40:09 +0000

    Added staging fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,9 @@ set :application, 'bip'
 set :repo_url, 'git@github.com:unepwcmc/bip.git'
 
 
+set :nvm_type, :user # or :system, depends on your nvm setup
+set :nvm_node, 'v12.18.3'
+set :nvm_map_bins, %w{node npm yarn}
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -28,21 +31,21 @@ set :scm_username, "unepwcmc-read"
 set :rvm_type, :user
 set :rvm_ruby_version, '2.3.1'
 
-namespace :npm do
-  desc 'Install dependencies with npm'
-  task :install do
-    on roles(:web) do
-      within release_path do
-        execute "bash -c 'source ~/.nvm/nvm.sh && cd '#{release_path}' && npm install'"
-      end
-    end
-  end
-end
+#namespace :npm do
+#  desc 'Install dependencies with npm'
+#  task :install do
+#    on roles(:web) do
+#      within release_path do
+#        execute "bash -c 'source ~/.nvm/nvm.sh && cd '#{release_path}' && npm install'"
+#      end
+#    end
+#  end
+#end
 
 
 #before 'deploy:starting', 'npm:install'
 
-before 'deploy:compile_assets', 'npm:install'
+#before 'deploy:compile_assets', 'npm:install'
 
 
 set :ssh_options, {
