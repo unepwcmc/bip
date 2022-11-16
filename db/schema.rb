c21671517bbb8ae9efab77b1ab8b17fb766fc957
Will Kocur <willkocur@gmail.com>
2022-11-16 10:27:11 +0000

    feat: add migration to rename resources tabs and update tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221114190902) do
+ActiveRecord::Schema.define(version: 20221116092629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
