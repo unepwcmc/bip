5c896c2043feeff017ed565de4b7d6204dc681ad
Will Kocur <willkocur@gmail.com>
2022-11-21 08:56:25 +0000

    refactor: remove unused instance variable

--- app/controllers/biodiversity_indicators_controller.rb
+++ app/controllers/biodiversity_indicators_controller.rb
@@ -5,7 +5,6 @@ class BiodiversityIndicatorsController < ApplicationController
     @indicators = Comfy::Cms::Page.find_by_label('Indicators')
                                   .descendants
                                   .sort_by(&:label)
-    @total_count = @indicators.size
     @indicators = paginate_results(@indicators, params[:page], params[:per_page])
   end
 end
