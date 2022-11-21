7e86605642f182299d3131158c46ea6b58380394
Will Kocur <willkocur@gmail.com>
2022-11-21 08:54:35 +0000

    refactor: use pagination from concern and partial

--- app/controllers/biodiversity_indicators_controller.rb
+++ app/controllers/biodiversity_indicators_controller.rb
@@ -1,21 +1,11 @@
 class BiodiversityIndicatorsController < ApplicationController
+  include Paginatable
+
   def index
     @indicators = Comfy::Cms::Page.find_by_label('Indicators')
                                   .descendants
                                   .sort_by(&:label)
     @total_count = @indicators.size
     @indicators = paginate_results(@indicators, params[:page], params[:per_page])
-  end
-
-  private
-
-  def paginate_results(results, page, per_page)
-    @per_page = (per_page || 10).to_i
-    @page = [0, page.to_i].max
-
-    @from = @page * @per_page
-    @to = @from + (@per_page - 1)
-
-    results[@from..@to] || []
   end
 end
