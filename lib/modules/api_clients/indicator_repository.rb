1d49fb985b8aac338a3dd8f57980759ec6dbd095
lucacug <luca.cugliari@unep-wcmc.org>
2022-11-29 14:31:32 +0000

    chore: move indicator repository api base url over to secrets

--- lib/modules/api_clients/indicator_repository.rb
+++ lib/modules/api_clients/indicator_repository.rb
@@ -4,13 +4,13 @@ module ApiClients::IndicatorRepository
   class << self
     include HTTParty
 
-    BASE_URL = 'http://localhost:3000'
+    BASE_URL = Rails.application.secrets.indicator_api_url # 'http://localhost:3000'
     INDICATOR_TYPE = %w[headline component complementary]
 
     def list(indicator_name)
       INDICATOR_TYPE.each do |type|
         response = HTTParty.post(BASE_URL + "/#{type}_indicators/api/v1" + '?items_per_page=250')
-        indicator = response['items'].select {|item| item['indicator_name'] == indicator_name}
+        indicator = response['items'].select { |item| item['indicator_name'] == indicator_name }
 
         break indicator if indicator.any?
       end
