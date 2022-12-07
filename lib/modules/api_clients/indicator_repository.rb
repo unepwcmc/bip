3baefa35d575a2aac82ef48a2ae1a220e960d453
lucacug <luca.cugliari@unep-wcmc.org>
2022-12-07 14:06:53 +0000

    chore: change constant name to reflect its nature(array)

--- lib/modules/api_clients/indicator_repository.rb
+++ lib/modules/api_clients/indicator_repository.rb
@@ -4,11 +4,11 @@ module ApiClients::IndicatorRepository
   class << self
     include HTTParty
 
-    BASE_URL = Rails.application.secrets.indicator_api_url # 'http://localhost:3000'
-    INDICATOR_TYPE = %w[headline component complementary]
+    BASE_URL = Rails.application.secrets.indicator_api_url
+    INDICATOR_TYPES = %w[headline component complementary]
 
     def list(indicator_name)
-      INDICATOR_TYPE.each do |type|
+      INDICATOR_TYPES.each do |type|
         response = HTTParty.post(BASE_URL + "/#{type}_indicators/api/v1" + '?items_per_page=250')
         indicator = response['items'].select { |item| item['indicator_name'] == indicator_name }
 
