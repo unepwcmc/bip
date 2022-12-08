a67c1f65f2997d7aa8d0a683eba9a04dc3fe0d89
lucacug <luca.cugliari@unep-wcmc.org>
2022-12-08 22:58:52 +0000

    feat: add regex to deal with indicator name mismatch between BIP and post2020

--- lib/modules/api_clients/indicator_repository.rb
+++ lib/modules/api_clients/indicator_repository.rb
@@ -8,12 +8,22 @@ module ApiClients::IndicatorRepository
     INDICATOR_TYPES = %w[headline component complementary]
 
     def list(indicator_name)
+      indicator = {}
       INDICATOR_TYPES.each do |type|
         response = HTTParty.post(BASE_URL + "/#{type}_indicators/api/v1" + '?items_per_page=250')
-        indicator = response['items'].select { |item| item['indicator_name'] == indicator_name }
+        indicator = response['items'].find { |item| indicator_name == sanitize_indicator_name(item['indicator_name']) }
 
-        break indicator if indicator.any?
+        return indicator if indicator.present?
       end
+      indicator
+    end
+
+    # this is a workaround to select the correct(the headline one) indicator name
+    # from the API where multiple indicators share the same name but
+    # the number/id prefix e.g.:
+    # A.3 Red List Index(headline), Red List index(component), Red List index(complementary)
+    def sanitize_indicator_name(name)
+      name.split(' ').first =~ /\d/ ? name.split(' ')[1..-1].join(' ') : name
     end
   end
 end
